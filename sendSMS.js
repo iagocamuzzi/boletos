51a6c587aa93c3b511554203f88ccb85f4856fdb
iagocamuzzi <iagocamuzzi2@gmail.com>
2024-08-27 09:25:19 -0300

    Update sendSMS.js

--- sendSMS.js
+++ sendSMS.js
@@ -1,34 +1,36 @@
-export default async function handler(req, res) {
+import unirest from "unirest";
+
+export default function handler(req, res) {
     if (req.method === 'POST') {
         const { numero, mensagem } = req.body;
-        try {
-            const response = await fetch('https://apihttp.disparopro.com.br:8433/mt', {
-                method: 'POST',
-                headers: {
-                    'Authorization': `Bearer 32c7568bbc8d079ea71d1dd1fa45a202dbcc20bb`,
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify([
-                    {
-                        numero,
-                        servico: "short",
-                        mensagem,
-                        parceiro_id: "5034e65a0c",
-                        codificacao: "0"
-                    }
-                ])
-            });
 
-            if (response.ok) {
-                const data = await response.json();
-                res.status(200).json({ message: 'SMS enviado com sucesso', data });
-            } else {
-                const errorData = await response.json();
-                res.status(response.status).json({ message: 'Erro ao enviar SMS', errorData });
+        const request = unirest("POST", "https://apihttp.disparopro.com.br:8433/mt");
+
+        request.headers({
+            "content-type": "application/json",
+            "authorization": "Bearer 32c7568bbc8d079ea71d1dd1fa45a202dbcc20bb"
+        });
+
+        request.type("json");
+        request.send([
+            {
+                "numero": numero,
+                "servico": "short",
+                "mensagem": mensagem,
+                "parceiro_id": "5034e65a0c",
+                "codificacao": "0"
             }
-        } catch (error) {
-            res.status(500).json({ message: 'Erro no servidor', error });
-        }
+        ]);
+
+        request.end(function (response) {
+            if (response.error) {
+                console.error(response.error);
+                res.status(500).json({ message: 'Erro ao enviar SMS', error: response.error });
+            } else {
+                console.log(response.body);
+                res.status(200).json({ message: 'SMS enviado com sucesso', data: response.body });
+            }
+        });
     } else {
         res.status(405).json({ message: 'Método não permitido' });
     }
